42a702a0569fb71484a66e92345166ff646168a1
Adao Oliveira <adaum79@gmail.com>
2020-06-02 11:28:15 -0300

    Quarta atualização da função parse_csv, implementando o parâmetro has_header.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,40 +4,48 @@
 import csv
 
 
-def parse_csv(filename: str, select=None, types=None) -> list:
+def parse_csv(filename: str, select=None, types=None, has_header=False) -> list:
     """
     Parse a CSV file in a list of records.
 
     :param filename:
-    :return: list:
+    :param select: A list of fields to filter
+    :param types: A list of types to typecasting
+    :return:
     """
     with open(filename) as f:
         rows = csv.reader(f)
 
-        headers = next(rows)
-
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
-
         records = []
+
+        if has_header:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
+
         for row in rows:
             if not row:
                 continue
 
-            if indices:
-                if types:
-                    row = [func(row[index]) for func, index in zip(types, indices)]
-                else:
-                    row = [row[index] for index in indices]
+            if has_header:
+                if indices:
+                    if types:
+                        row = [func(row[index]) for func, index in zip(types, indices)]
+                    else:
+                        row = [row[index] for index in indices]
+                record = dict(zip(headers, row))
+            else:
+                record = row
 
-            record = dict(zip(headers, row))
             records.append(record)
 
         return records
 
 
 if __name__ == '__main__':
-    print(parse_csv('Data/portfolio.csv', select='name price'.split(), types=[str, float]))
+    #print(parse_csv(filename='Data/portfolio.csv', has_header=True))
+    #print(parse_csv(filename='Data/portfolio.csv', select='name shares'.split(), types=[str, int], has_header=True))
+    print(parse_csv(filename='Data/prices.csv'))
