abdd9fa492b1e7646f0bafa36e121bb9a13129bb
Adao Oliveira <adaum79@gmail.com>
2020-06-02 09:04:09 -0300

    Segunda atualização da função parse_csv, seguindo o algorítmo sugerido no livro.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,7 +4,7 @@
 import csv
 
 
-def parse_csv(filename: str, selected_cols=[]) -> list:
+def parse_csv(filename: str, select=None) -> list:
     """
     Parse a CSV file in a list of records.
 
@@ -17,24 +17,21 @@ def parse_csv(filename: str, selected_cols=[]) -> list:
         # Read de file headers
         headers = next(rows)
 
-        # Check if selected_cols are in headers
-        sel_cols = []
-        if selected_cols:
-            sel_cols = [c for c in selected_cols if c in headers]
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
 
         records = []
         for row in rows:
             if not row:
                 continue
 
+            if indices:
+                row = [row[index] for index in indices]
+
             record = dict(zip(headers, row))
-
-            if sel_cols:
-                my_record = {}
-                for col in sel_cols:
-                    my_record[col] = record[col]
-                record = my_record
-
             records.append(record)
 
         return records
