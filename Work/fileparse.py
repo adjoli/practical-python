229212ac1817a367abb11907daa293473271b50d
Adao Oliveira <adaum79@gmail.com>
2020-06-02 05:57:55 -0300

    Implementação da primeira versão de fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,25 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+
+def parse_csv(filename: str) -> list:
+    """
+    Parse a CSV file in a list of records.
+
+    :param filename:
+    :return: list:
+    """
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read de file headers
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+        return records
