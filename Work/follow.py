0ef94f6085dd7db8b1238e63b8f68b6cfbe4906a
Adao Oliveira <adaum79@gmail.com>
2020-06-04 14:15:23 -0300

    Criação da função 'follow()', que funciona como um generator, fornecendo as novas linhas no final do arquivo passado como parâmetro.

--- Work/follow.py
+++ Work/follow.py
@@ -2,17 +2,27 @@ import os
 import time
 
 print(os.getcwd())
-f = open('stocklog.csv')
-f.seek(0, os.SEEK_END)
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)
-        continue
-    fields = line.split(',')
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
+def follow(filename):
+    f = open(filename, 'r')
+    f.seek(0, os.SEEK_END)
+    while True:
+        line = f.readline()
+        if line == '':
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+if __name__ == '__main__':
+    import report
+
+    portfolio = report.read_portfolio('portfolio.csv')
+
+    for line in follow('stocklog.csv'):
+        fields = line.split(',')
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if name in portfolio:
+            print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
