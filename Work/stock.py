505cf50484a7b0308d3a2731e100cb2576c47db7
Adao Oliveira <adaum79@gmail.com>
2020-06-05 13:53:12 -0300

    Criação do arquivo typedproperty.py, da função typedproperty() e das definições lambda String, Integer e Float.

--- Work/stock.py
+++ Work/stock.py
@@ -1,29 +1,14 @@
+from typedproperty import typedproperty, String, Integer, Float
+
 class Stock:
-    #__slots__ = ('name', 'shares', 'price')  # Restrict attributes
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._shares = value
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, value):
-        if not isinstance(value, float):
-            raise TypeError('Expected float')
-        self._price = value
 
     @property
     def cost(self):
@@ -35,18 +20,3 @@ class Stock:
 
     def __repr__(self):
         return f"Stock({self.name}, {self.shares}, {self.price})"
-
-
-def main():
-    import fileparse
-    with open('Data/portfolio.csv') as lines:
-        portdicts = fileparse.parse_csv(lines, select='name shares price'.split(), types=[str, int, float])
-
-        portfolio = [Stock(s['name'], s['shares'], s['price']) for s in portdicts]
-
-        print(portfolio)
-
-        print(sum(s.cost for s in portfolio))
-
-if __name__ == '__main__':
-    main()
